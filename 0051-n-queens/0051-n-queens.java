c7f36ba33d4688b06615109e18f8a85ad5291da3
Indhuthamarai <125845927+Indhuthamarai@users.noreply.github.com>
2025-01-03 11:35:02 +0530

    Time: 6 ms (21.06%), Space: 44.9 MB (61.2%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.java
@@ -0,0 +1,46 @@
+class Solution {
+    public boolean canPlace(int r,int c,List<String> cur){
+        for(int row=r-1;row>=0;row--){
+            if(cur.get(row).charAt(c)=='Q'){
+                return false;
+            }
+        }
+        for(int row=r-1,col=c-1;row>=0 && col>=0;row--,col--){
+            if(cur.get(row).charAt(col)=='Q'){
+                return false;
+            }
+        }
+        for(int row=r-1,col=c+1;row>=0 && col<cur.size();row--,col++){
+            if(cur.get(row).charAt(col)=='Q'){
+                return false;
+            }
+        }
+        return true;
+
+    }
+    public void backtrack(int r,List<List<String>> res,List<String> cur){
+        if(r==cur.size()){
+            res.add(new ArrayList<>(cur));
+            return;
+        }
+        for(int col=0;col<cur.size();col++){
+            if(canPlace(r,col,cur)){
+                StringBuilder sb=new StringBuilder(cur.get(r));
+                sb.setCharAt(col,'Q');
+                cur.set(r,sb.toString());
+                backtrack(r+1,res,cur);
+                sb.setCharAt(col,'.');
+                cur.set(r,sb.toString());
+            }
+        }
+    }
+    public List<List<String>> solveNQueens(int n) {
+        List<List<String>> res=new ArrayList<>();
+        List<String> cur=new ArrayList<>();
+        for(int i=0;i<n;i++){
+            cur.add(".".repeat(n));
+        }
+        backtrack(0,res,cur);
+        return res;
+    }
+}
