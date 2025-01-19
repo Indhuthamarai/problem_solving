42d807a8571f280ca27aea4eb96e9ce0678676c1
indhuja12 <indhujat.22cse@kongu.edu>
2025-01-19 22:00:51 +0530

    Time: 32 ms (5.27%), Space: 45.4 MB (23.33%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.java
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.java
@@ -1,7 +1,6 @@
 class Solution {
     public int minEatingSpeed(int[] piles, int h) {
         int n=piles.length;
-        int res=0;
         Arrays.sort(piles);
         int l=1,hi=piles[n-1];
         while(l<hi){
@@ -9,13 +8,11 @@ class Solution {
             int m=l+(hi-l)/2;
             for(int j=0;j<n;j++){
                 sum+=Math.ceil((double)piles[j]/m);
-                // if(sum>h){
-                //     break;
-                // }
+                
             }
             if(sum<=h){
                 hi=m;
-                // res=Math.min(res,m);
+               
             }
             else{
                 l=m+1;
