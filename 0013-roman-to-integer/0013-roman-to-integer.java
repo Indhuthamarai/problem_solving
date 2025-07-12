a66f42e82c406111a20b4e36d1f849e425468883
Indhuja T <indhujat.22cse@kongu.edu>
2025-07-12 16:37:44 +0530

    Time: 8 ms (6.51%), Space: 45.4 MB (5.09%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,31 +1,24 @@
 class Solution {
     public int romanToInt(String s) {
-        int result=0;
-        for(int i=0;i<s.length();i++){
-            switch(s.charAt(i)){
-                case 'I': result=result+1;
-                    break;
-                case 'V': result=result+5;
-                    break;
-                case 'X': result+=10;
-                    break;
-                case 'L': result+=50;
-                    break;
-                case 'C': result+=100;
-                    break;
-                case 'D': result+=500;
-                    break;
-                case 'M': result+=1000;
-                    break;
-            }
-        }   
-        if(s.contains("IV")||s.contains("IX"))
-            result=result-2;
-        if(s.contains("XL")||s.contains("XC"))
-            result=result-20;
-        if(s.contains("CD")||s.contains("CM"))
-            result=result-200;
-       
-        return result;
+         Map<Character, Integer> Roman = Map.of(
+        'I', 1,
+        'V', 5,
+        'X', 10,
+        'L', 50,
+        'C', 100,
+        'D', 500,
+        'M', 1000
+        );
+        if(s.length() == 0) return 0;
+        if(s.length() == 1) return Roman.get(s.charAt(0));
+
+        Integer first = Roman.get(s.charAt(0));
+        Integer second = Roman.get(s.charAt(1));
+
+        if (first < second) {
+            return second - first + romanToInt(s.substring(2));
+        } else {
+            return first + romanToInt(s.substring(1));
+        }
     }
 }
