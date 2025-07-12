782ad04101434c5f4570dfe6a9e5231151ec76de
Indhuja T <indhujat.22cse@kongu.edu>
2025-07-12 16:23:03 +0530

    Time: 3 ms (79.38%), Space: 44.4 MB (96.81%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,25 +1,31 @@
 class Solution {
     public int romanToInt(String s) {
-        int number=0;
-        HashMap<Character,Integer> map=new HashMap<>();
-        int a[]={1,5,10,50,100,500,1000};
-        char[] c={'I','V','X','L','C','D','M'};
-        for(int i=0;i<7;i++)
-        {
-            map.put(c[i],a[i]);
-        }
-        number+=map.get(s.charAt(s.length()-1));
-        for(int i=s.length()-2;i>=0;i--)
-        {
-            if(((s.charAt(i+1)=='V'||s.charAt(i+1)=='X')&&(s.charAt(i)=='I'))||((s.charAt(i+1)=='L'||s.charAt(i+1)=='C')&&(s.charAt(i)=='X')||((s.charAt(i+1)=='D'||s.charAt(i+1)=='M')&&s.charAt(i)=='C')))
-            {
-                number-=map.get(s.charAt(i));
+        int result=0;
+        for(int i=0;i<s.length();i++){
+            switch(s.charAt(i)){
+                case 'I': result=result+1;
+                    break;
+                case 'V': result=result+5;
+                    break;
+                case 'X': result+=10;
+                    break;
+                case 'L': result+=50;
+                    break;
+                case 'C': result+=100;
+                    break;
+                case 'D': result+=500;
+                    break;
+                case 'M': result+=1000;
+                    break;
             }
-            else{
-            number+=map.get(s.charAt(i));
-            }
-
-        }
-        return number;
+        }   
+        if(s.contains("IV")||s.contains("IX"))
+            result=result-2;
+        if(s.contains("XL")||s.contains("XC"))
+            result=result-20;
+        if(s.contains("CD")||s.contains("CM"))
+            result=result-200;
+       
+        return result;
     }
 }
