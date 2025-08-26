c7736e480d291b324268641dc5d032bdbbf74c46
Indhuja T <indhujat.22cse@kongu.edu>
2025-08-26 20:10:57 +0530

    Time: 421 ms (7.09%), Space: 0B (100%) - LeetHub

--- 1801-average-time-of-process-per-machine/1801-average-time-of-process-per-machine.sql
+++ 1801-average-time-of-process-per-machine/1801-average-time-of-process-per-machine.sql
@@ -1,1 +1,1 @@
-select a1.machine_id,round(avg(a2.timestamp-a1.timestamp),3) as processing_time from(select * from Activity where activity_type='start')a1,(select * from Activity where activity_type='end')a2 where a1.machine_id=a2.machine_id and a1.process_id=a2.process_id group by machine_id;
+select a1.machine_id,round(avg(a2.timestamp-a1.timestamp),3) as processing_time from Activity a1 join Activity a2 on a1.machine_id=a2.machine_id and a1.process_id=a2.process_id and a1.activity_type='start' and a2.activity_type='end' group by a1.machine_id;
