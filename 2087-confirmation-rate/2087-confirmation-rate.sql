4fe33089dee9f631886295fdecc9754e22226e63
Indhuja T <indhujat.22cse@kongu.edu>
2025-08-27 12:59:53 +0530

    Time: 594 ms (91.95%), Space: 0B (100%) - LeetHub

--- 2087-confirmation-rate/2087-confirmation-rate.sql
+++ 2087-confirmation-rate/2087-confirmation-rate.sql
@@ -1,9 +1,11 @@
-SELECT
+SELECT 
     s.user_id,
     ROUND(
-        AVG(
-            CASE 
-                WHEN c.action = 'confirmed' THEN 1.00
-                ELSE 0
-            END),2) confirmation_rate
-FROM Signups s LEFT JOIN Confirmations c ON s.user_id = c.user_id GROUP BY s.user_id;
+        COALESCE(SUM(c.action = 'confirmed') * 1.0 
+                 / NULLIF(SUM(c.action IN ('confirmed','timeout')), 0), 0),
+        2
+    ) AS confirmation_rate
+FROM Signups s
+LEFT JOIN Confirmations c 
+  ON s.user_id = c.user_id
+GROUP BY s.user_id;
