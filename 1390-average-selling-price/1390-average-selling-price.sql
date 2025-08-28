a73a981b5f85d2e228789e1c94ced5cdf54b9ec9
Indhuja T <indhujat.22cse@kongu.edu>
2025-08-28 18:51:05 +0530

    Time: 1431 ms (5.1%), Space: 0B (100%) - LeetHub

--- 1390-average-selling-price/1390-average-selling-price.sql
+++ 1390-average-selling-price/1390-average-selling-price.sql
@@ -1,3 +1,3 @@
 select p.product_id,
-       coalesce(round(sum(u.units*p.price)/sum(u.units),2),0)
+       coalesce(round(sum(u.units*p.price)/nullif(sum(u.units),0),2),0)
        as average_price from Prices p left join UnitsSold u on p.product_id=u.product_id and u.purchase_date between p.start_date and p.end_date group by p.product_id;
