ff5dcebbca4ffa68be77faaf16be147e010a8d78
Indhuja T <indhujat.22cse@kongu.edu>
2025-08-30 22:33:17 +0530

    Time: 1041 ms (38.79%), Space: 0B (100%) - LeetHub

---
+++ 1773-percentage-of-users-attended-a-contest/1773-percentage-of-users-attended-a-contest.sql
@@ -0,0 +1,1 @@
+select contest_id, round(((count(contest_id)/(select count(distinct user_id) from users))*100),2) as percentage from Register group by contest_id order by percentage desc,contest_id asc;
